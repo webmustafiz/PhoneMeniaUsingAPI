97928161c70d68709440b597be2d8e794f3e1dfc
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-10-16 08:41:43 +0800

    addd some

--- utilities.js
+++ utilities.js
@@ -2,5 +2,15 @@ const processSearch = (datalimit) => {  // 9 no work use data limit becouse how 
     toggoleSpiner(true) // 8 under the toggole spiner
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText, datalimit);
+    if (searchText.length) {
+        loadPhones(searchText, datalimit);
+    }
+    else {
+        swal.fire({
+        title: 'Warning!',
+        text: 'Please Search Mobile Phone',
+        icon: 'warning',
+        confirmButtonText: 'Cancel'
+        })
+    }
 }
